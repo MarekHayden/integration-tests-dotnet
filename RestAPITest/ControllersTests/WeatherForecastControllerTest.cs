6a47a9b1d1638448c9adc7332fb8308ef90a344e
MarekHayden <marek.hayden@gjensidige.no>
2024-11-29 09:48:41 +0100

    successful test

---
+++ RestAPITest/ControllersTests/WeatherForecastControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using RestAPI;
+using System.Web;
+
+namespace RestAPITest.ControllersTests
+{
+    public class WeatherForecastControllerTest : IClassFixture<RestAPIFactory>
+    {
+        private readonly HttpClient _client;
+        public WeatherForecastControllerTest(RestAPIFactory factory)
+        {
+            _client = factory.HttpClient;
+        }
+
+        [Fact]
+        public async Task Get()
+        {
+            var uriBuilder = new UriBuilder("https://localhost:7059/WeatherForecast");
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            uriBuilder.Query = query.ToString();
+            var requestUri = uriBuilder.ToString();
+            var response = await _client.GetAsync(requestUri);
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var forecasts = JsonConvert.DeserializeObject<List<WeatherForecast>>(responseContent);
+
+            Assert.Equal(5, forecasts.Count);
+            Assert.True(forecasts.All(f => f.Date != default));
+            Assert.True(forecasts.All(f => f.TemperatureC >= -20 && f.TemperatureC <= 55));
+        }
+    }
+}
---
+++ RestAPITest/RestAPIFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace RestAPITest
+{
+    public class RestAPIFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        public HttpClient HttpClient { get; private set; } = default!;
+
+        public async Task InitializeAsync()
+        {
+            HttpClient = CreateClient();
+        }
+
+        public Task DisposeAsync() => Task.CompletedTask;
+    }
+}
